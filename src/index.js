9f24ae2fb0e52b34be638e33c805c555fa2f1a89
TrevPennington <43477475+TrevPennington@users.noreply.github.com>
2020-12-01 16:39:41 -0500

    server connection and test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql
+} from "@apollo/client";
 
 import App from "./App";
 
+const client = new ApolloClient({
+  uri: "https://5sh2x.sse.codesandbox.io/",
+  cache: new InMemoryCache()
+});
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   rootElement
 );
+
+client
+  .query({
+    query: gql`
+      query {
+        allLinks {
+          id
+          url
+        }
+      }
+    `
+  })
+  .then((result) => console.log(result));
